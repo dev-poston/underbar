72eab435d493304a7388dd8f9304db56f3d6053b
Devon Poston <dev.poston@gmail.com>
2020-08-25 17:40:40 -0700

    fix for .each funtion

--- src/underbar.js
+++ src/underbar.js
@@ -38,7 +38,18 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
-    return n === undefined ? array[array.length - 1] : array.slice(0, n);
+    if (n === undefined) {
+      return array[array.length - 1];
+    }
+    if (n === 0) {
+      return [];
+    }
+    if (n > array.length) {
+      return array;
+    }
+
+    return array.slice(array.indexOf(n));
+
   };
 
   // Call iterator(value, key, collection) for each element of collection.
@@ -47,6 +58,15 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    if (Array.isArray(collection)) {
+      for (var i = 0; i < collection.length; i++) {
+        iterator(collection[i], i, collection);
+      }
+    } else {
+      for (var key in collection) {
+        iterator(collection[key], key, collection);
+      }
+    }
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
